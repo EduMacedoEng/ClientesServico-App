d8f07c20e0aa1c3668073a25bc041295ee70f9b2
Eduardo Macedo <eduardomacedo.eng@hotmail.com>
2020-10-04 17:03:41 -0300

    Adicionando as rotas da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent} // Aqui eu digo que quando eu acessa /home eu vou para este componente
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
