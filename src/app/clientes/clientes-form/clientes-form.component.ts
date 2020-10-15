f4fc6fdfdc447153467a2a9bfc73d8a7be7a2af3
Eduardo Macedo <eduardomacedo.eng@hotmail.com>
2020-10-15 10:57:26 -0300

    Criado o método que atualiza os registros.

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../cliente';
 import { ClientesService } from '../../clientes.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-clientes-form',
@@ -13,16 +14,43 @@ export class ClientesFormComponent implements OnInit {
   cliente: Cliente;
   success: boolean = false;
   errors: String[];
+  id: number;
 
-  constructor(private service: ClientesService, private router: Router) {
+  constructor(
+    private service: ClientesService, 
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
     this.cliente = new Cliente();
   }
 
   ngOnInit(): void {
+    let params: Observable<Params> = this.activatedRoute.params;
+    params.subscribe(urlParams => {
+      this.id = urlParams['id'];
+      if (this.id) {
+        this.service
+          .getClienteById(this.id)
+          .subscribe(
+            response => this.cliente = response,
+            errorResponse => this.cliente = new Cliente()
+          )
+      }
+    })
   }
 
   onSubmit(){
-    this.service
+    if (this.id){
+      this.service
+        .atualizar(this.cliente)
+        .subscribe(response => {
+          this.success = true;
+          this.errors = null;
+        }, errorResponse => {
+          this.errors = ['Erro ao atualizar o cliente.']
+        })
+    } else {
+      this.service
       .salvar(this.cliente)
       .subscribe(response => {
         this.success = true;
@@ -34,6 +62,8 @@ export class ClientesFormComponent implements OnInit {
 			    // | errors é a lista dos erros dentro da propriedade error
           this.errors = errorResponse.error.errors;
           });
+    }
+    
   }
 
   voltarParaListagem(){
